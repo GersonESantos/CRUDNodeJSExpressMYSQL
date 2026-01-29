c8672f88c1477ba2a2b649a5fec76255d49f2f82
GersonESS <gebhsantos@gmail.com>
2026-01-29 01:44:58 -0300

    a

---
+++ regis/10/backend/api-tasks/src/database/db.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+const connectToDb = () => {
+  mongoose
+    .connect(process.env.DB_URI)
+    .then(() => console.log("MongoDB Atlas CONECTADO!"))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectToDb;
