0ee43665fdd25a6f783b3b6fa8bcda2cc31e0721
GersonESS <gebhsantos@gmail.com>
2026-01-28 22:54:20 -0300

    a

--- regis/05/backend/api-tasks/src/middlewares/requireApiKey.js
+++
@@ -1,9 +0,0 @@
-// Middleware simples para checar API Key
-module.exports = (req, res, next) => {
-  const apiKey = req.header('x-api-key');
-  if (apiKey && apiKey === '123456') {
-    next();
-  } else {
-    res.status(401).json({ error: 'API Key inválida ou ausente' });
-  }
-};
